0a1aee93b119d2d037fd1a8e48d7c1f867c54d62
장민종 <alswhddl10@naver.com>
2021-02-21 23:17:50 +0900

    코팅 테스트 이름: KClosest PriorityQueue을 사용하여 정렬을 통해 제곱수가 0,0에 가장 가까운 배열을 찾는 문제

---
+++ src/test/KCloset_Test.java
@@ -0,0 +1,53 @@
+package test;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class KCloset_Test {
+
+    public static void main(String[] args) {
+        int[][] points = {{1, 3}, {-2, 2}};
+        int k = 1;
+
+        int[][] result = solve(points, k);
+
+        print(result);
+    }
+
+    private static int[][] solve(int[][] points, int k) {
+
+
+        Queue<int[]> queue = new PriorityQueue<>(points.length, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] a, int[] b) {
+                return ((a[0] * a[0]) + (a[1] * a[1])) - ((b[0] * b[0]) + (b[1] * b[1]));
+            }
+        });
+
+        int[][] result = new int[k][2];
+        int index = 0;
+
+        for (int[] tmp : points) {
+            queue.offer(tmp);
+        }
+
+        while (index < k) {
+            result[index] = queue.poll();
+            index++;
+        }
+
+        return result;
+    }
+
+
+    private static void print(int[][] tmp) {
+
+        for (int i = 0; i < tmp.length; i++) {
+            for (int j = 0; j < tmp[i].length; j++) {
+                System.out.println(tmp[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
