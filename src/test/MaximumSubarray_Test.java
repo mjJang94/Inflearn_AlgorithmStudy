fdd2b40bd5b8925c368713b6c5d59534a6605a72
장민종 <alswhddl10@naver.com>
2021-02-24 22:02:46 +0900

    코팅 테스트 이름: MaximumSubarray 배열의 원소들 중 합했을때 나올수 있는 최대 숫자 구하기

---
+++ src/test/MaximumSubarray_Test.java
@@ -0,0 +1,25 @@
+package test;
+
+public class MaximumSubarray_Test {
+
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        System.out.println(maxSubArray(nums));
+    }
+
+    private static int maxSubArray(int[] nums) {
+        //1
+        int max = nums[0];
+        int newSum = nums[0];
+
+        //2
+        for (int i = 1; i < nums.length; i++){
+
+            newSum = Math.max(nums[i], newSum + nums[i]);
+            max = Math.max(newSum, max);
+        }
+
+        return max;
+    }
+}
