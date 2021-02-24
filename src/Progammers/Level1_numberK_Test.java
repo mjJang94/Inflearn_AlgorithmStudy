ba633f24c6888d5052a297e8ae6c23138b6e0ab3
장민종 <alswhddl10@naver.com>
2021-02-24 21:21:39 +0900

    코팅 테스트 이름: K번째 수 배열과 2차원배열을 받아 조건식을 구하고 조건에 따라 배열을 구한다.

---
+++ src/Progammers/Level1_numberK_Test.java
@@ -0,0 +1,35 @@
+package Progammers;
+
+import java.util.Arrays;
+
+public class Level1_numberK_Test {
+
+    public static void main(String[] args) {
+
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+
+        System.out.println(Arrays.toString(solution(array, commands)));
+    }
+
+    static int[] solution(int[] array, int[][] commands) {
+        //1.
+        int[] answer = new int[commands.length];
+
+
+        //2.
+        for (int i = 0; i < commands.length; i++){
+
+            int start = commands[i][0];
+            int end = commands[i][1];
+            int position = commands[i][2];
+
+            int[] tmp = Arrays.copyOfRange(array, start - 1, end);
+            Arrays.sort(tmp);
+
+            answer[i] = tmp[position - 1];
+        }
+
+        return answer;
+    }
+}
