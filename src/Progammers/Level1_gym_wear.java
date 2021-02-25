84dc84c4912ac6500c89871b1d265d75f4044ecb
PayMint_MJ <alswhddl10@naver.com>
2021-02-25 18:32:50 +0900

    코팅 테스트 이름: 체육복

---
+++ src/Progammers/Level1_gym_wear.java
@@ -0,0 +1,54 @@
+package Progammers;
+
+public class Level1_gym_wear {
+
+
+    public static void main(String[] args) {
+
+//        int n = 5;
+        int n = 3;
+//        int[] lost = {2,4};
+        int[] lost = {3};
+//        int[] reserve = {1,3,5};
+        int[] reserve = {1};
+
+        System.out.println(solution(n, lost, reserve));
+
+    }
+
+    private static int solution(int n, int[] lost, int[] reserve) {
+        //1
+        int answer = n - lost.length;
+
+        //2 여벌옷이 있는사람들중 도난당한사람 거르기
+        for (int i = 0; i < lost.length; i++) {
+            for (int j = 0; j < reserve.length; j++) {
+
+
+                if (lost[i] == reserve[j]) {
+                    answer++;
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                    break;
+                }
+
+            }
+        }
+
+        //3 여벌옷도 없고 도난도 당했지만 구제될수 있는사람 고르기
+        for (int i = 0; i < lost.length; i++) {
+            for (int j = 0; j < reserve.length; j++) {
+
+                if (lost[i] == reserve[j] -1 || lost[i] == reserve[j] +1){
+                    answer++;
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                    break;
+                }
+            }
+        }
+
+
+        return answer;
+    }
+}
--- src/Progammers/Level1_numberK.java
+++ src/Progammers/Level1_numberK.java
@@ -9,27 +9,31 @@ public class Level1_numberK {
         int[] array = {1, 5, 2, 6, 3, 7, 4};
         int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
 
+        //output
+        //[5,6,3]
+
         System.out.println(Arrays.toString(solution(array, commands)));
     }
 
     static int[] solution(int[] array, int[][] commands) {
-        //1.
+
         int[] answer = new int[commands.length];
 
-        //2.
+
+
         for (int i = 0; i < commands.length; i++){
 
             int start = commands[i][0];
             int end = commands[i][1];
             int position = commands[i][2];
-            //copyOfRange (원본 배열, 복사 시작 인덱스 (배열이기 떄문에 -1), 복사 끝 인덱스)
+
             int[] tmp = Arrays.copyOfRange(array, start - 1, end);
 
             Arrays.sort(tmp);
 
-            //배열이기 떄문에 또 -1
             answer[i] = tmp[position - 1];
         }
+
 
         return answer;
     }
