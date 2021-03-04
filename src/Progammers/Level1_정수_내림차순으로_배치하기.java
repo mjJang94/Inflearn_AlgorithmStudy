55a483c85f26fa4abec095344adfd44419a4b183
장민종 <alswhddl10@naver.com>
2021-03-04 23:52:17 +0900

    코팅 테스트 이름: 정수 내림차순으로 배치하기

--- src/Progammers/Level1_정수_내림차순으로_배치하기.java
+++ src/Progammers/Level1_정수_내림차순으로_배치하기.java
@@ -1,11 +1,14 @@
 package Progammers;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class Level1_정수_내림차순으로_배치하기 {
 
     public static void main(String[] args) {
 
         long n = 118372;
-
 
         System.out.println(solution(n));
     }
@@ -14,9 +17,27 @@ public class Level1_정수_내림차순으로_배치하기 {
 
     public static long solution(long n){
         long answer = 0;
+        String tmp = String.valueOf(n);
+        StringBuilder sum = new StringBuilder();
+        Character[] chars = new Character[tmp.length()];
 
 
+        for (int i = 0; i < tmp.length(); i++){
+            chars[i] = tmp.charAt(i);
+        }
 
+        Arrays.sort(chars, new Comparator<Character>() {
+            @Override
+            public int compare(Character o1, Character o2) {
+                return o2 - o1;
+            }
+        });
+
+        for (int i = 0; i < chars.length; i++){
+            sum.append(chars[i]);
+        }
+
+        answer = Long.parseLong(sum.toString());
         return answer;
     }
 }
