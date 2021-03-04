40a5000de37f2fe3bcb4bf9422c3f1b2be682c83
PayMint_MJ <alswhddl10@naver.com>
2021-03-04 18:32:28 +0900

    코팅 테스트 이름: 정수 내림차순으로 배치하기

---
+++ src/Progammers/Level1_정수_내림차순으로_배치하기.java
@@ -0,0 +1,22 @@
+package Progammers;
+
+public class Level1_정수_내림차순으로_배치하기 {
+
+    public static void main(String[] args) {
+
+        long n = 118372;
+
+
+        System.out.println(solution(n));
+    }
+
+
+
+    public static long solution(long n){
+        long answer = 0;
+
+
+
+        return answer;
+    }
+}
