097175624a991f8c5582cf41c88633a49d84f494
PayMint_MJ <alswhddl10@naver.com>
2021-03-08 16:43:29 +0900

    코팅 테스트 이름: 키패드 누르기

--- src/Progammers/Level1_키패드_누르기.java
+++ src/Progammers/Level1_키패드_누르기.java
@@ -16,20 +16,19 @@ public class Level1_키패드_누르기 {
 
         //1
         StringBuilder answer = new StringBuilder();
-        int leftHandIndex = 0;
-        int rightHandIndex = 0;
-//        Stack<String> stack = new Stack<>();
+        int leftHandIndex = 10;
+        int rightHandIndex = 12;
 
 
         //2
-        for (int num: numbers){
+        for (int num : numbers) {
 
             switch (num) {
 
                 //왼쪽 열
                 case 1:
                 case 4:
-                case 7:{
+                case 7: {
                     leftHandIndex = num;
                     answer.append("L");
                     break;
@@ -38,7 +37,7 @@ public class Level1_키패드_누르기 {
                 //오른쪽 열
                 case 3:
                 case 6:
-                case 9:{
+                case 9: {
                     rightHandIndex = num;
                     answer.append("R");
                     break;
@@ -48,31 +47,48 @@ public class Level1_키패드_누르기 {
                 case 2:
                 case 5:
                 case 8:
-                case 0:{
+                case 0: {
 
-                    if (num + 1 == rightHandIndex && num -1 == leftHandIndex){
+                    int leftHandDistance = getLength(leftHandIndex, num);
+                    int rightHandDistance = getLength(rightHandIndex, num);
 
-                        if (hand.equals("right")){
-                            answer.append("R");
-                        }else{
-                            answer.append("L");
-                        }
+                    if (leftHandDistance > rightHandDistance){
+
+                        rightHandIndex = num;
+                        answer.append("R");
+                    }else if (leftHandDistance < rightHandDistance){
+
+                        leftHandIndex = num;
+                        answer.append("L");
                     }else{
 
-                        int leftHandDistance = num - leftHandIndex;
-                        int rightHandDistance = num - rightHandIndex;
-
-                        if (rightHandDistance > leftHandDistance){
+                        if (hand.equals("right")){
+                            rightHandIndex = num;
+                            answer.append("R");
+                        }else{
+                            leftHandIndex = num;
                             answer.append("L");
                         }
-
-                        if (leftHandDistance > rightHandDistance){
-                            answer.append("R");
-                        }
                     }
+
+                    break;
                 }
             }
         }
         return answer.toString();
+    }
+
+    public static int getLength(int index, int num) {
+
+        index = (index == 0) ? 11 : index;
+        num = (num == 0) ? 11 : num;
+
+        int x = (index - 1) / 3;
+        int y = (index - 1) % 3;
+
+        int numberX = num /3;
+        int numberY = 1;
+
+        return Math.abs(x-numberX) + Math.abs(y-numberY);
     }
 }
